c31fad75a980bc97c57e148a76c9b80fd82019a0
danonel <dldm13@gmail.com>
2021-10-13 11:26:01 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.spec.ts'],
+  clearMocks: true,
+  // setupFiles: ['dotenv/config'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
