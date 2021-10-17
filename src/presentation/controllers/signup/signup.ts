4bfec23045e200cac24a7e10721c44b754386eaf
danonel <dldm13@gmail.com>
2021-10-17 11:08:39 -0300

    chore: jest unit and intregration test

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,36 +1,47 @@
+import {
+  EmailValidator,
+  Controller,
+  AddAccount,
+  HttpRequest,
+  HttpResponse
+} from './signup-protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, ok, serverError } from '../../helpers/http-helper'
-import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols'
+import { badRequest, serverError, ok } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
+
   constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
     this.addAccount = addAccount
   }
 
-  async handle (httpRequest: HttpRequest): Promise< HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation'
+      ]
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
-
       const { name, email, password, passwordConfirmation } = httpRequest.body
-
       if (password !== passwordConfirmation) {
-        return badRequest(new MissingParamError('passwordConfirmation'))
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      const isEmailValid = this.emailValidator.validate(email)
-      if (!isEmailValid) {
+      const isValid = this.emailValidator.validate(email)
+      if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
       const account = await this.addAccount.add({
-        email, name, password
+        email,
+        name,
+        password
       })
       return ok(account)
     } catch (error) {
