c8e1a0cae253bf03c8df661e22fa9d40f2c8aee0
danonel <dldm13@gmail.com>
2021-10-17 11:57:08 -0300

    feat: ensure DbAccountRepository calls AddAccountRepository with correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,6 @@
+import { AddAccountModel } from '../../domain/useCase/add-account'
+import { AccountModel } from '../../domain/model/account-model'
+
+export interface AddAccountRepository {
+  add: (accountData: AddAccountModel) => Promise<AccountModel>
+}
--- src/data/useCases/add-account/db-add-account-protocols.ts
+++ src/data/useCases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/model/account-model'
 export * from '../../../domain/useCase/add-account'
 export * from '../../protocols/encrypter'
+export * from '../../protocols/add-account-repository'
