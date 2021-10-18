a13965a9c4548b0532442bbc3b072d4e6fdfcf5b
danonel <dldm13@gmail.com>
2021-10-18 18:05:52 -0300

    refactor: lint

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,18 +3,18 @@ import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   client: null as MongoClient,
   uri: null as string,
-  async connect(uri: string): Promise<void> {
+  async connect (uri: string): Promise<void> {
     this.uri = uri
     this.client = await MongoClient.connect(uri)
   },
 
-  async disconnect(): Promise<void> {
+  async disconnect (): Promise<void> {
     await this.client.close()
   },
-  getCollection(name: string): Collection {
+  getCollection (name: string): Collection {
     return this.client.db().collection(name)
   },
-  map(collection: any): any {
+  map (collection: any): any {
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, collectionWithoutId, {
       id: _id.toHexString()
