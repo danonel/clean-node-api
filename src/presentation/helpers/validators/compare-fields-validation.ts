25c0675a1eb520f941e314ce41ec81e1c5c33102
danonel <dldm13@gmail.com>
2021-10-19 15:25:26 -0300

    fix: remove validation comparation error

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -4,13 +4,13 @@ import { Validation } from './validation'
 export class CompareFieldsValidation implements Validation {
   private readonly fieldName: string
   private readonly fieldToCompare: string
-  constructor (fieldName: string, fieldToCompare) {
+  constructor (fieldName: string, fieldToCompare: string) {
     this.fieldName = fieldName
     this.fieldToCompare = fieldToCompare
   }
 
   validate (input: any): Error {
-    if (this.fieldName !== this.fieldToCompare) {
+    if (input[this.fieldName] !== input[this.fieldToCompare]) {
       return new InvalidParamError(this.fieldToCompare)
     }
   }
