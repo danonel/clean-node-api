e4d9da646a76d5b0592d5371a9f2010d216818dd
danonel <dldm13@gmail.com>
2021-10-19 11:25:19 -0300

    test: ensure LogincController returns 500 if authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,6 @@
-import { Authentication } from '../../../domain/useCase/authentication'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, serverError, unauthorazied } from '../../helpers/http-helper'
-import { EmailValidator, HttpRequest } from '../signup/signup-protocols'
+import { EmailValidator, Authentication, HttpRequest } from './login-protols'
 import { LoginController } from './login'
 
 const makeEmailValidator = (): EmailValidator => {
@@ -109,5 +108,12 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
 
     expect(httpResponse).toEqual(unauthorazied())
+  })
+  test('should return 500 if authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
